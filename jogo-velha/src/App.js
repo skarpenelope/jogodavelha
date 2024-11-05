3ef1a7917025c4cb847907c2a38e606a37f60889
skarpenelope <penelopeskarlate@gmail.com>
2024-11-05 15:24:45 -0300

    quase dando certo o commit

--- jogo-velha/src/App.js
+++ jogo-velha/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+
 function App() {
 
   class Quadrado extends React.Component{
@@ -27,21 +28,21 @@ function App() {
       return (
         <div>
           <div className='tabu-linhas'>
-            {this.render.square(0)}
-            {this.render.square(1)}
-            {this.render.square(2)}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
           </div> 
 
           <div className='tabu-linhas'>
-            {this.render.square(3)}
-            {this.render.square(4)}
-            {this.render.square(5)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
           </div> 
 
           <div className='tabu-linhas'>
-            {this.render.square(6)}
-            {this.render.square(7)}
-            {this.render.square(8)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
           </div>
         </div>
       );
@@ -52,17 +53,70 @@ function App() {
 
   class Jogo extends React.Component{
     constructor(props) {
-      super(props)
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsnext: true,
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      if (squares[i] || this.calcularGanhador(squares)) return;
+      squares[i] = this.state.xIsnext ? "X" : "O";
+      this.setState({ squares: squares, xIsnext: !this.state.xIsnext});
+    }
+
+    calcularGanhador(squares) {
+      const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+
+      ];
+
+      for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+          return squares[a];
+      }
+      return null;
+
+      render() ;{ // Método render
+        const winner = this.calcularGanhador(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Vencedor: ' + winner;
+        } else {
+          status = 'Próximo jogador: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+    
+        return (
+          <div>
+            <h1>{status}</h1> {/* Mostra o status do jogo */}
+            <Tabuleiro 
+              squares={this.state.squares} 
+              onClick={(i) => this.handleClick(i)} 
+            />
+          </div>
+        );
+      }
     }
 
   }
   return (
     <div className="App">
 
-      <h1> olá </h1>
+      <Jogo/>
       
     </div>
   );
 }
 
-export default App;
+export default App
+
